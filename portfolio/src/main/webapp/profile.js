8250f9d077ce2615d1f0ae3b17b9e7cff154c44e
Michael <mvalverde2019@gmail.com>
2020-07-10 16:19:14 -0400

    preload input form to enhance user experience.

--- portfolio/src/main/webapp/profile.js
+++ portfolio/src/main/webapp/profile.js
@@ -3,7 +3,6 @@ function updateUsername() {
     method: 'POST',
     body: new URLSearchParams(`username=${document.getElementById('username').value}`)
   };
-  console.l
   fetch(`/profile`, parameters)
       .then(response => response.json())
       .then(response => {
@@ -17,9 +16,11 @@ function updateUsername() {
   return false;
 }
 
-
 function giveUserSuccess() {
-  giveUserFeedback('Your username has been updated!', '#00ff00');
+  giveUserFeedback(`
+    <p>Your username has been updated!</p>
+    <p><a href="/">Click here</a> to go back to the home page</p>`,
+    '#00ff00');
 }
 
 function giveUserFailure() {
@@ -33,5 +34,18 @@ function giveUserFeedback(message, color) {
   let feedbackDiv = document.getElementById('user-feedback');
 
   feedbackDiv.style['background-color'] = color;
-  feedbackDiv.innerText = message;
+  feedbackDiv.innerHTML = message;
 };
+
+function populateUsername() {
+  fetch('/auth')
+      .then(response => response.json())
+      .then(authJson => {
+        let usernameInput = document.getElementById('username');
+        if (authJson.username == "") {
+          usernameInput.placeholder = "Enter username here."
+        } else {
+          usernameInput.value = authJson.username;
+        }
+      });
+}
